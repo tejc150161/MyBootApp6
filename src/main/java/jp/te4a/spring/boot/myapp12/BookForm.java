9cdd9b5fe6d966e6d9b1887e831df2e60c012189
150161 <150161@e0601-04.JC-21.local>
2018-05-30 14:36:21 +0900

    MYBootApp12_2018/05/30

--- src/main/java/jp/te4a/spring/boot/myapp12/BookForm.java
+++ src/main/java/jp/te4a/spring/boot/myapp12/BookForm.java
@@ -16,7 +16,7 @@ public class BookForm {
 	@EcheckValid(param = "abc")
 	private String title;
 	@Size(min=3, max=20)
-	@Writter(ok ="宮田愛萌")
+	@Writter(ok ="東北タロウ")
 	private String writter;
 	private String publisher;
 	@Min(0)
